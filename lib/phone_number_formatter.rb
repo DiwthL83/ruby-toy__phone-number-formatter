b3c9c901ee9012860a2b30c21d62ef14afb18ceb
Jerry Samuels Jr <jsamuelsjr@gmail.com>
2016-06-26 22:02:29 -0500

    Updated the function with my definition to convert phone number string to a formatted phone number.

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -1,6 +1,14 @@
 # This method takes a string like `"4122226644"` and
 # returns a properly formatted phone number.
 
+
 def format_phone_number(phone_number_str)
-  return phone_number_str
+
+	p_n_s = phone_number_str.to_s
+
+	#Use  each substring of the phone number string to create formatted phone number.
+	ph_frmtd = "(" + p_n_s[0..2] + ")" + p_n_s[3..5] + "-" + p_n_s[6..9]
+	
+	return phone_number_str
+
 end
